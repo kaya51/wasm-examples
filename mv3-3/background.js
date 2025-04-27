07a316a6169b050439cb8c1312c83ba610dd7fd7
kaya51 <147663417+kaya51@users.noreply.github.com>
2025-04-27 19:25:06 +0000

    add demo code

--- mv3-3/background.js
+++ mv3-3/background.js
@@ -1,7 +1,30 @@
-import { initializeImageMagick } from './magick-wasm/dist/index.js';
+import {
+	initializeImageMagick,
+	ImageMagick,
+	Magick,
+	MagickFormat,
+	Quantum,
+} from './magick-wasm/dist/index.js';
 
 const magickLocation = chrome.runtime.getURL('magick-wasm/dist/magick.wasm');
 
 fetch(magickLocation)
 	.then(response => response.bytes())
-	.then(bytes => initializeImageMagick(bytes));
+	.then(bytes => initializeImageMagick(bytes))
+	.then(() => {
+		console.log(Magick.imageMagickVersion);
+		console.log('Delegates:', Magick.delegates);
+		console.log('Features:', Magick.features);
+		console.log('Quantum:', Quantum.depth);
+
+		console.log('');
+		ImageMagick.read('logo:', image => {
+			image.resize(100, 100);
+			image.blur(1, 5);
+			console.log(image.toString());
+
+			image.write(MagickFormat.Jpeg, data => {
+				console.log(data.length);
+			});
+		});
+	});
