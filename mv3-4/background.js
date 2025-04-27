770b35be9eb7cc4e3f4ee86fe151d9954f4c8ef1
kaya51 <147663417+kaya51@users.noreply.github.com>
2025-04-27 19:22:56 +0000

    remove mv3-4

--- mv3-4/background.js
+++
@@ -1,7 +0,0 @@
-import { initializeImageMagick } from './magick-wasm/dist/index.js';
-
-const magickLocation = chrome.runtime.getURL('magick-wasm/dist/magick.wasm');
-
-fetch(magickLocation)
-	.then(response => response.bytes())
-	.then(bytes => initializeImageMagick(bytes));
